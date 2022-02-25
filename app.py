9c5b2ee8569e541cd03fd8fb7464730a5b1069f0
FelicityPaterson <91577881+FelicityPaterson@users.noreply.github.com>
2022-02-25 21:36:02 +1100

    Scaffolded start of user input

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("hello World")
+def storeValues(Name, Expiry, Category, Location):
+    print("store: {0}, {1}, {2}, {3}".format(Name,Expiry,Category,Location))
+
+
+def nonFreshFoodInput():
+    name = input("Name of Item ")
+    expiry = input("Enter expiry in format dd-mm-yyyy ")
+    category = input("Category ")
+    location = input("Storage location ")
+    storeValues(name,expiry,category,location)
+
+
+
+loopvar = True
+while loopvar == True:
+   usrInput = input("Enter command: F for Fresh food, N for Non-Fresh food, Fr for frozen, L for leftovers, Q for Exit ").upper()
+   if usrInput == "F":
+       print("Fresh food")
+   elif usrInput == "N":
+       nonFreshFoodInput()
+   elif usrInput == "FR":
+       print("frozen food")
+   elif usrInput == "L":
+       print("leftovers")
+   elif usrInput == "Q":
+       loopvar = False
